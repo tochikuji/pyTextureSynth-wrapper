fcb45bc4ce55b3acf4fb2f8008b2d722766526a2
Aiga SUZUKI <ai-suzuki@aist.go.jp>
2016-10-23 16:02:28 +0900

    fix import namespace issue

--- texturesynth/__init__.py
+++ texturesynth/__init__.py
@@ -1,7 +1,5 @@
 import wrapper
-import texture_param
 import util
 
 TextureSynth = wrapper.TextureSynth
-TextureParam = texture_param.TextureParam
 Util = util
--- texturesynth/texture_param.py
+++ texturesynth/texture_param.py
@@ -5,34 +5,3 @@ TextureParam
 steerable pyramid texture parameters
 
 """
-
-from util import ml2np
-
-
-class TextureParam(object):
-
-    def __init__(self, ml_param=None):
-
-        if ml_param is None:
-            self.varianceHPR = None
-            self.cousinRealCorr = None
-            self.parentRealCorr = None
-            self.pixelStats = None
-            self.autoCorrReal = None
-            self.pixelLPStats = None
-            self.autoCorrMag = None
-            self.cousinMagCorr = None
-            self.parentMagCorr = None
-            self.magMeans = None
-
-        else:
-            self.varianceHPR = ml2np(ml_param['varianceHPR'])
-            self.cousinRealCorr = ml2np(ml_param['cousinRealCorr'])
-            self.parentRealCorr = ml2np(ml_param['parentRealCorr'])
-            self.pixelStats = ml2np(ml_param['pixelStats'])
-            self.autoCorrReal = ml2np(ml_param['autoCorrReal'])
-            self.pixelLPStats = ml2np(ml_param['pixelLPStats'])
-            self.autoCorrMag = ml2np(ml_param['autoCorrMag'])
-            self.cousinMagCorr = ml2np(ml_param['cousinMagCorr'])
-            self.parentMagCorr = ml2np(ml_param['parentMagCorr'])
-            self.magMeans = ml2np(ml_param['magMeans'])
